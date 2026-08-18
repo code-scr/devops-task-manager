3fb0b39f5f57bcadecea1e134c3f1015039cf26b
sujith <sujith@gmail.com>
2026-08-18 12:04:48 +0530

    Fix frontend and backend task manager

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 const db = mysql.createPool({
